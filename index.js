02e248555deb593614f14f1f586906abd14397b4
Cory Forsyth <cory.forsyth@gmail.com>
2015-03-23 22:07:56 -0400

    listen to port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response){
   response.render('index');
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(app.get('port'), function(){
   var host = server.address().address;
   var port = server.address().port;
 
