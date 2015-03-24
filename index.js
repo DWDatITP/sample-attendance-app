718c4aedf76c9066630926388bad98bc3445c578
Cory Forsyth <cory.forsyth@gmail.com>
2015-03-23 22:01:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(request, response){
+  response.render('index');
+});
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
